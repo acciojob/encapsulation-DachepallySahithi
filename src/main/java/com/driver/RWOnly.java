dd436ebeefe47e543221e67e611888040a02a51c
DachepallySahithi <dachepallysahithi@gmail.com>
2024-11-13 11:27:46 +0530

    added extra lines

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    public RWOnly(String name){
+        this.name = name;
+    }
+    public String getNAme(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
 }
