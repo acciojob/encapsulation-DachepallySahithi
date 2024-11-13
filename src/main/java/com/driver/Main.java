d4cda1fb4706be79ed665d3d9d30968fdb07dbaa
DachepallySahithi <dachepallysahithi@gmail.com>
2024-11-13 12:19:30 +0530

    added lines

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[]){
+        RWOnly object = new RWOnly();
+        object.setName("Jeh");
+        System.out.println(object.getName());
+    }
 }
